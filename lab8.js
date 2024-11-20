cb7ce10ee68c91119a728e201450820526bffbaa
Cmb9315 <cmb93@njit.edu>
2024-11-20 11:35:48 -0500

    Got Rid of Revit

---
+++ lab8.js
@@ -0,0 +1,101 @@
+// Q1
+let name = "Colman Brackisn";
+let age = 21;
+console.log("Name:", name);
+console.log("Age:", age);
+
+// Q2
+let stringVar = "My Name is colman!";
+let numberVar = 42;
+let booleanVar = true;
+let undefinedVar;
+    
+console.log("String:", stringVar);
+
+console.log("Number:", numberVar);
+
+
+console.log("Boolean:", booleanVar);
+console.log("Undefined:", undefinedVar);
+
+// Q3
+
+let RollNumber = 15;
+
+
+if (RollNumber > 10) {
+    console.log("The RollNumber is greater than 10");
+}
+
+// Q4
+let isMember = true;
+
+if (isMember) {
+    console.log("Member discount applied.");
+}
+
+// Q5
+let startValue = 50;
+
+
+if (startValue > 0) {
+    console.log("The value is positive.");
+} else if (startValue < 0) {
+    console.log("The value is negative.");
+} else {
+    console.log("The value is zero.");
+}
+
+// Q6
+let a = 10, b = 20;
+
+console.log("a == b:", a == b);
+console.log("a === b:", a === b);
+console.log("a != b:", a != b);
+console.log("a > b:", a > b);
+console.log("a < b:", a < b);
+console.log("a >= b:", a >= b);
+console.log("a <= b:", a <= b);
+
+// Q7
+let firstName = "Colman";
+let lastName = "Brackin";
+
+let userAge = 21;
+console.log(`Hello, ${firstName} ${lastName}, you are ${userAge} years old.`);
+
+// Q8
+let num = 50;
+
+if (num > 0 && num < 100) {
+    console.log("The number is within range.");
+}
+
+// Q9
+let hasCar = true,
+ hasLicense = true, 
+ hasInsurance = true;
+
+if (hasCar && hasLicense && hasInsurance) {
+    console.log("You can drive legally.");
+} else {
+    console.log("Check your driving eligibility.");
+}
+
+// Q10
+
+num = 21;
+if (num % 2 === 0) {
+    console.log("The number is even.");
+} else {
+    console.log("The number is odd.");
+}
+
+// Q11
+
+let score = 85;
+if (score >= 90) {
+    console.log("Grade A.");
+} else if (score >= 80 && score <= 89) {
+
+}
---
+++ scriptone.js
@@ -0,0 +1,41 @@
+//var age = 20;
+
+//var age; 
+
+let age = 20;
+
+console.log(age);
+
+let username = "Harry"
+
+console.log(username);
+
+let pet = true;
+
+console.log(pet);
+
+let house; null;
+ 
+console.log(house);
+
+let bignumber = 2.123113131313131333424324
+
+console.log(bigNumber)
+
+
+
+
+
+
+
+
+
+//let rollNo = 21;
+
+//rollNo= 101;
+
+//console.log(rolNo);
+
+//const valuePi = 3.14;
+
+//console.log(valuePi); 
